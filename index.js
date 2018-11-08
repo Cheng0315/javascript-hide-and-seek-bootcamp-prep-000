eaacd4b245f5352fcf208857cbaef127fbbbb44a
Cheng Vue <cvu3@ucsc.edu>
2018-11-08 05:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function deepestChild() {
+  return document.
+}
